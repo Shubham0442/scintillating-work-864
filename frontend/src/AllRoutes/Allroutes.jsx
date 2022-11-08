4a5d475956837efc977e8f4b04b4a47f4dbbad08
Shubham0442 <shubhamrakhonde507@gmail.com>
2022-11-08 19:35:36 +0530

    created routes in AllRoutes, updated the index.js

--- frontend/src/AllRoutes/Allroutes.jsx
+++ frontend/src/AllRoutes/Allroutes.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Login from '../Pages/Login'
+import Signup from '../Pages/Signup'
+import SingleVideoGallery from '../Pages/SingleVideoGallery'
 
 const Allroutes = () => {
   return (
     <div>
-        
+        <Routes>
+           <Route path={"/"} element={<Home/>}></Route>
+           <Route path={"/login"} element={<Login/>}></Route>
+           <Route path={"/signup"} element={<Signup/>}></Route>
+           <Route path={"/singlevideogallery"} element={<SingleVideoGallery/>}></Route>
+        </Routes>
     </div>
   )
 }
---
+++ frontend/src/Pages/SingleVideoGallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleVideoGallery = () => {
+  return (
+    <div>SingleVideoGallery</div>
+  )
+}
+
+export default SingleVideoGallery
