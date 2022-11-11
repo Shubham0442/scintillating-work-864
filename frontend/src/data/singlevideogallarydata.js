40c48561602d39381626384bc4b348458ffe9a1f
RAJENDRA YADAV <rjyadav0230@gmail.com>
2022-11-11 14:06:20 +0530

    Related and cast-crew added

---
+++ frontend/src/data/singlevideogallarydata.js
@@ -0,0 +1,228 @@
+const obj = [
+  {
+    episodes: [
+      {
+        id: 1,
+        epi: "EPISODE 1",
+        img: "https://is2-ssl.mzstatic.com/image/thumb/r8WioQPptoS1zWhCz_stsw/1478x832.webp",
+        title: "Light Out",
+        desc: "When the U.S. government finally tracks them down, the off-the-grid Fox family flees to begin a new life.",
+        duration: "57 min",
+        release_date: "Apr 30",
+      },
+      {
+        id: 2,
+        epi: "EPISODE 2",
+        img: "https://is4-ssl.mzstatic.com/image/thumb/5Du6a3w-KZmaMAseFSe-Vw/1478x832.webp",
+        title: "foxes and coyotes",
+        desc: "The Foxes enlist the help of two coyotes who promise to take them across the U.S. border into Mexico.",
+        duration: "55 min",
+        release_date: "Apr 30",
+      },
+      {
+        id: 3,
+        epi: "EPISODE 3",
+        img: "https://is1-ssl.mzstatic.com/image/thumb/AL4KeAShfOftYDU2PpRE9g/1478x832.webp",
+        title: "Everybody Knows This Is Nowhere",
+        desc: "With the Feds closing in on them, the Foxes and Chuy must cross the desert on foot—and face adversity both natural and man-made.",
+        duration: "47 min",
+        release_date: "May 7",
+      },
+      {
+        id: 4,
+        epi: "EPISODE 4",
+        img: "https://is4-ssl.mzstatic.com/image/thumb/5Ic1cP9utxvz-uBmEssh9A/1478x832.webp",
+        title: "Bus stop",
+        desc: "The Foxes seek refuge in an opulent hacienda, where a family’s all-powerful matriarch sees them as pawns in her own game.",
+        duration: "53 min",
+        release_date: "May 14",
+      },
+      {
+        id: 5,
+        epi: "EPISODE 5",
+        img: "https://is1-ssl.mzstatic.com/image/thumb/y73ipreK3gMXTpmKCNBUKw/1478x832.webp",
+        title: "Elvis, Jesus, Coca-Cola",
+        desc: "Arriving in Mexico City, the Fox parents attempt to connect with Allie’s mysterious contact while the kids get into trouble.",
+        duration: "49 min",
+        release_date: "May 21",
+      },
+      {
+        id: 6,
+        epi: "EPISODE 6",
+        img: "https://is3-ssl.mzstatic.com/image/thumb/W0VZ9__hoaDH8J0O0NvbAA/1478x832.webp",
+        title: "Calaca",
+        desc: "Held captive by a shadowy organization, Allie and Margot must find a way to escape and get back to their kids.",
+        duration: "42 min",
+        release_date: "May 28",
+      },
+      {
+        id: 7,
+        epi: "EPISODE 7",
+        img: "https://is3-ssl.mzstatic.com/image/thumb/xyULr4ZjJ1zrxTWgPNkldg/1478x832.webp",
+        title: "The Glass Sandwich",
+        desc: "Season finale. The Foxes are forced into a crisis that will either bring them back together or tear them apart forever.",
+        duration: "56 min",
+        release_date: "Jul 4",
+      },
+    ],
+    casts: [
+      {
+        id: 1,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/fOKH_5kj7Y792CBiFnC8kg/492x492ve.webp",
+        original_name: "Justin Theroux",
+        cast_name: "Allie Fox",
+      },
+      {
+        id: 2,
+        img: "https://is5-ssl.mzstatic.com/image/thumb/TBDo4AO2mIKY1yrYdJbbeQ/492x492ve.webp",
+        original_name: "Melissa George",
+        cast_name: "Margot Fox",
+      },
+      {
+        id: 3,
+        img: "https://is5-ssl.mzstatic.com/image/thumb/3NMyzOQuR5bxbiVDhX3y7w/492x492ve.webp",
+        original_name: "Logan Polish",
+        cast_name: "Dina Fox",
+      },
+      {
+        id: 4,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/wT4BiHDMPn3aY_CxWbwldg/492x492ve.webp",
+        original_name: "Gaberial Bateman",
+        cast_name: "Charlie Fox",
+      },
+      {
+        id: 5,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/A3GB7WVPfX6Z30Bv4JnjUg/492x492bb.webp",
+        original_name: "Neil Cross",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 6,
+        img: "https://is5-ssl.mzstatic.com/image/thumb/m1qM4FBe4qN9f8OAc7ox9g/492x492bb.webp",
+        original_name: "Rupert Wyatt",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 7,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/nd6qrra08q7ot2YLZhvi_w/492x492ve.webp",
+        original_name: "Edward L. McDonnell",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 8,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/gb9baFpEa94p3mWZvcPIig/492x492bb.webp",
+        original_name: "Justin Theroux",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 9,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/vLflCbjhVtF7JXFe2xDNWw/492x492ve.webp",
+        original_name: "Bob Bookman",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 10,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/auXlMqH2JRpF7Kb2d8bTBw/492x492bb.webp",
+        original_name: "Alan Gasmer",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 11,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/eZRW_l3oxkhGtxR74RfOTQ/492x492bb.webp",
+        original_name: "Peter Jaysen",
+        cast_name: "Executive Producer",
+      },
+      {
+        id: 12,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/iq0PjpISz60PW5RZCBBhbA/492x492bb.webp",
+        original_name: "Paul Theroux",
+        cast_name: "Executive Producer",
+      },
+    ],
+
+    related_contents: [
+      {
+        id: 1,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/AWDRdQz0nepFpnsUNiTDuw/738x416.webp",
+      },
+      {
+        id: 2,
+        img: "https://is4-ssl.mzstatic.com/image/thumb/L4WNJNV3nbmyAL6MQSeGuw/738x416.webp",
+      },
+      {
+        id: 3,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/oANBVngpEJDvHRhdyozySA/738x416.webp",
+      },
+      {
+        id: 4,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/8593WFOGVTTA1ncIGrL37g/738x416.webp",
+      },
+      {
+        id: 5,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/GT66HoV8VJL1IZwaqos_TQ/738x416.webp",
+      },
+      {
+        id: 6,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/D1SCGzM1ESBh-SIkHwcvUQ/738x416.webp",
+      },
+      {
+        id: 7,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/Epzt9WNlE21aE-FNCoVj2w/738x416.webp",
+      },
+      {
+        id: 8,
+        img: "https://is5-ssl.mzstatic.com/image/thumb/7SeRlnCzKlgeqrg6-ixkig/738x416.webp",
+      },
+      {
+        id: 9,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/DCj5c2CndQsWCj9zkGGa5Q/738x416.webp",
+      },
+      {
+        id: 10,
+        img: "https://is5-ssl.mzstatic.com/image/thumb/KdlcNbBUOtf7sUXH5z9N8A/738x416.webp",
+      },
+      {
+        id: 11,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/qYEYYvFXN2O4zEuCcBSbjw/738x416.webp",
+      },
+      {
+        id: 12,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/3iAbZQ4pznOpDRenkvOhNA/738x416.webp",
+      },
+      {
+        id: 13,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/6MFkEmAhzwoAZfThq0xWHA/738x416.webp",
+      },
+      {
+        id: 14,
+        img: "https://is5-ssl.mzstatic.com/image/thumb/KS0pkmrVEdO4uYZA5CpRZA/738x416.webp",
+      },
+      {
+        id: 15,
+        img: "https://is2-ssl.mzstatic.com/image/thumb/nMny0tR8aTkNRR3FHWQcAw/738x416.webp",
+      },
+      {
+        id: 16,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/04JyAIWtM1q-61kfjf6BsQ/738x416.webp",
+      },
+      {
+        id: 17,
+        img: "https://is4-ssl.mzstatic.com/image/thumb/2kwDdUMkGYVufGBF3g1hdA/738x416.webp",
+      },
+      {
+        id: 18,
+        img: "https://is3-ssl.mzstatic.com/image/thumb/ZYv1Yo9zcK029EjWlyRSYg/738x416.webp",
+      },
+      {
+        id: 19,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/fwOVLEMoZhr-wkppMR12XA/738x416.webp",
+      },
+      {
+        id: 20,
+        img: "https://is1-ssl.mzstatic.com/image/thumb/6fu6YRITQhblYxAGDfPR3A/738x416.webp",
+      },
+    ],
+  },
+];
+
+export default obj;
